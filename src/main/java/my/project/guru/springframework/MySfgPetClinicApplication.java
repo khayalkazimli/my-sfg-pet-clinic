74d5a9e278885ccd39761084dc55e8b378781f1f
Khayal Kazimli <khayalkazimli@gmail.com>
2022-02-15 12:47:02 +0400

    added project

---
+++ src/main/java/my/project/guru/springframework/MySfgPetClinicApplication.java
@@ -0,0 +1,13 @@
+package my.project.guru.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySfgPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySfgPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/my/project/guru/springframework/MySfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package my.project.guru.springframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
